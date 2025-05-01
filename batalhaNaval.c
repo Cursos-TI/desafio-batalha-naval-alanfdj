7c085660d8a62e338519b4b195a75e2846df664e
alanfdj <alanfdj2024@gmail.com>
2025-05-01 13:57:04 +0000

    POSICIONA NAVIOS NA DIAGONAL

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -39,48 +39,48 @@ int main() {
 
 
     //MONTA TABULEIRO
-  int tabuleiro[10][10] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+  int tabuleiro[10][10]; 
 
-  };
+  //VARIAVEIS QUE REPRESENTAM OS NAVIOS
+  int navio1[3] = {3, 3, 3};
+  int navio2[3] = {3, 3, 3};
+  int navio1diagonal[3] = {3, 3, 3};
+  int navio2diagonal[3] = {3, 3, 3};
 
 
-//VARIAVEIS QUE REPRESENTAM OS NAVIOS
-int navio1[3] = {3, 3, 3};
-int navio2[3] = {3, 3, 3};
-
-//POSICIONA NAVIO NA HORIZONTAL
-tabuleiro[7][2] = navio1[0];
-tabuleiro[7][3] = navio1[1];
-tabuleiro[7][4] = navio1[2];
-
-//POSICIONA NAVIO NA VERTICAL
-tabuleiro[2][6] = navio2[0];
-tabuleiro[3][6] = navio2[1];
-tabuleiro[4][6] = navio2[2];
-
-
-//IMPRIME O TABULEIRO COM OS DOIS NAVIOS POSICIONADOS
+//IMPRIME O TABULEIRO COM OS NAVIOS POSICIONADOS
   for (int i = 0; i < 10; i++) { // Loop para as linhas
+   
+      
     for (int j = 0; j < 10; j++) { // Loop para as colunas
       
+      tabuleiro[i][j] = 0; //MONTA TABULEIRO ZERADO
+
+      //POSICIONA NAVIO NA HORIZONTAL
+      tabuleiro[7][2] = navio1[0];
+      tabuleiro[7][3] = navio1[1];
+      tabuleiro[7][4] = navio1[2];
+
+      //POSICIONA NAVIO NA VERTICAL
+      tabuleiro[2][6] = navio2[0];
+      tabuleiro[3][6] = navio2[1];
+      tabuleiro[4][6] = navio2[2];
+
+      //POSICIONA NAVIO 1 NA DIAGONAL
+      tabuleiro[1][1] = navio1diagonal[0];
+      tabuleiro[2][2] = navio1diagonal[1];
+      tabuleiro[3][3] = navio1diagonal[2];
+
+      //POSICIONA NAVIO 2 NA DIAGONAL
+      tabuleiro[7][9] = navio2diagonal[0];
+      tabuleiro[8][8] = navio2diagonal[1];
+      tabuleiro[9][7] = navio2diagonal[2];
+
       printf("%d ", tabuleiro[i][j]); // Imprime o elemento atual
     }
     printf("\n"); // Pula para a próxima linha para melhor visualização
   }
-
-
-
-
+  
 
 
 
