4a2eab8f4bae883f38d1ef2780b3504bb9925770
alanfdj <alanfdj2024@gmail.com>
2025-05-04 16:42:46 +0000

    IMPRIME AS HABILIDADES ESPECIAIS

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,44 +1,9 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+int main(){
 
 
-
-    //MONTA TABULEIRO
+  //MONTA TABULEIRO
   int tabuleiro[10][10]; 
 
   //VARIAVEIS QUE REPRESENTAM OS NAVIOS
@@ -76,6 +41,53 @@ int main() {
       tabuleiro[8][8] = navio2diagonal[1];
       tabuleiro[9][7] = navio2diagonal[2];
 
+
+      //HABILIDADES ESPECIAIS
+      if(i == 0){
+
+        tabuleiro[0][4] = 5;//PARTE SUPERIOR DO TRIANGULO
+        tabuleiro[0][8] = 5;//PARTE SUPERIOR DO OCTAEDRO
+
+      }else if(i == 1)
+      {
+
+        //PARTE INFERIOR DO TRIANGULO
+        tabuleiro[i][3] = 5;
+        tabuleiro[i][4] = 5;
+        tabuleiro[i][5] = 5;
+        
+        //PARTE CENTRAL DO OCTAEDRO
+        tabuleiro[i][7] = 5;
+        tabuleiro[i][8] = 5;
+        tabuleiro[i][9] = 5;
+
+      }else if(i == 2)
+      {
+        tabuleiro[i][8] = 5;//PARTE INFERIOR DO OCTAEDRO
+     
+      }else if(i == 3)//INICIO DA CRUZ
+      {
+        tabuleiro[i][2] = 5;      
+      
+      }else if(i == 4)
+      {
+        tabuleiro[i][0] = 5;
+        tabuleiro[i][1] = 5;
+        tabuleiro[i][2] = 5;
+        tabuleiro[i][3] = 5;
+        tabuleiro[i][4] = 5;
+
+      }else if(i == 5)
+      {
+        tabuleiro[i][2] = 5;
+      
+      }else if(i == 6)
+      {
+        tabuleiro[i][2] = 5;
+      }//FINAL DA CRUZ
+
+
+
       printf("%d ", tabuleiro[i][j]); // Imprime o elemento atual
     }
     printf("\n"); // Pula para a próxima linha para melhor visualização
@@ -83,7 +95,8 @@ int main() {
   
 
 
+  return 0;
 
 
-    return 0;
 }
+
